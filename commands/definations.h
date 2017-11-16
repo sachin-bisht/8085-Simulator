1e0a20825be31dbe17fe30d282502e9b9f3f78f8
sachin-bisht <sachinbisht939@gmail.com>
2017-11-16 21:17:01 +0530

    Create definations.h

---
+++ commands/definations.h
@@ -0,0 +1,30 @@
+void mov(string, string);
+void mvi(string, string);
+void lxi(string, string);
+void lda(string);
+void sta(string);
+void lhld(string);
+void shld(string);
+void stax(string);
+void xchg();
+
+void add(string);
+void adi(string);
+void sub(string);
+void inr(string);
+void dcr(string);
+void inx(string);
+void dcx(string);
+void dad(string);
+void sui(string);
+
+void cma();
+void cmp(string);
+
+void jmp(string);
+void jc(string);
+void jnc(string);
+void jz(string);
+void jnz(string);
+
+void setc(string, string);
